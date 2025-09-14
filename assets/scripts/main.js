67d7f7d847486a94118c62d452fc2183986e7ae1
coderlawa <5d12tpff@students.codeinstitute.net>
2025-09-14 21:35:32 +0100

    DOM elements added to JS file

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,2 +1,14 @@
 // API KEY
 import Config from "./config.js";
+
+// DOM Elements
+const flightForm = document.getElementById('flight-form');
+const departureInput = document.getElementById('departure');
+const destinationInput = document.getElementById('destination');
+const dateInput = document.getElementById('date');
+const passengersInput = document.getElementById('passengers');
+const flightGradeInputs = document.getElementsByName('flight-grade');
+const resultsContainer = document.getElementById('results-container');
+const loadingSpinner = document.getElementById('loading-spinner');
+const errorMessage = document.getElementById('error-message');
+
